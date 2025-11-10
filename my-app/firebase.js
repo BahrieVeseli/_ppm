1cb0703e0aef93cbea56af1ac26fe8477beef681
BahrieVeseli <bahrie.veseli@student.uni-pr.edu>
2025-11-10 19:57:09 +0100

    commit

---
+++ my-app/firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // ✅ Importo për autentikim
+
+// ⚙️ Konfigurimi yt Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCniTxiTOUtc0qv1BGlbCVnE8X6kjx4FTE",
+  authDomain: "fir-ecfbf.firebaseapp.com",
+  projectId: "fir-ecfbf",
+  storageBucket: "fir-ecfbf.firebasestorage.app",
+  messagingSenderId: "59661906063",
+  appId: "1:59661906063:web:d6c2be769ce8e454e080fa",
+  measurementId: "G-DWT75Z2EWV",
+};
+
+// 🔥 Inicializo Firebase
+const app = initializeApp(firebaseConfig);
+
+// ✅ Inicializo Auth (për login/signup)
+export const auth = getAuth(app);
